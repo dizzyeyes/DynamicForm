e11ca7e8f1e6d65a41ff755df32848b688fb73e6
dizzyeyes <909338300@qq.com>
2015-06-20 01:10:57 +0800

    change file names

---
+++ js/dynamicForm.js
@@ -0,0 +1,144 @@
+function dynamicForm(viewer,title,id){
+    var scope = this;
+    if(id==undefined) id="form-new";
+    this.viewer=viewer;
+    this.initForm(id);
+    this.visible=false;   
+    this.fadeTool= new fadeInOutTool();
+    this.enabled = true;
+    if(title==undefined) title='untitled';
+    this.title=title;
+    
+    document.dynamicform = this;
+   
+    document.addEventListener( 'mousemove', this.onMouseMove, false );
+} 
+
+dynamicForm.prototype.onMouseMove = function(event)
+{
+    this.dynamicform.fadeTool.onMoveDiv(event);
+}
+
+dynamicForm.prototype.initForm = function(id){
+    this.div = document.createElement("div");  
+    this.div.id=id;  
+    this.div.className="bs-form";
+    this.div.style.display="none";  
+    this.div.style.zIndex=100;
+    this.div.style.position="absolute";
+    document.body.appendChild(this.div);   
+}
+
+
+dynamicForm.prototype.fillForm = function(dataJson){
+        var form=this;
+        $('#'+this.div.id+'').bootstrapForm({
+            width: 340,
+            data: dataJson,
+            onSelect: function(name) {
+                if(name==undefined) return;
+                form.process(name);
+                form.hide();
+            }
+        });
+}
+dynamicForm.prototype.hide = function()
+{                
+    this.div.style.display="none";
+    this.clearForm();
+    this.visible=false;
+}
+dynamicForm.prototype.selectobj = function(obj)
+{          
+    this.obj=obj;
+}
+dynamicForm.prototype.show = function(x,y){
+    if(this.enabled==false) return;
+    this.clearForm();
+    var data=this.getJsonDataofForm();
+    this.fillForm(data);
+    this.fillActioin();
+    this.fadeTool.fadeIn(this.div);
+    if(x==undefined)
+        this.fadeTool.MoveFloatLayer(this.div.id);
+    else
+        this.fadeTool.MoveFloatLayer(this.div.id,x,y);
+    this.visible=true;
+}
+dynamicForm.prototype.clearForm = function (){
+    this.clearDiv(this.div);
+ }
+ 
+dynamicForm.prototype.clearDiv = function (div){
+    var chld = div.children;
+    for(var item=0;item<chld.length;item++)
+    {
+        div.removeChild(chld[item]);
+    }
+ }
+dynamicForm.prototype.getJsonDataofForm = function()
+{
+    var dataJson="[]";
+    switch(this.div.id)
+    {
+        case 'form-new':
+            dataJson=this.getJsonDataofFormNew();
+        break;
+    }
+    return dataJson;
+}
+
+dynamicForm.prototype.getJsonDataofFormNew = function()
+{
+    var formFirstlist=new Array();
+    formFirstlist.push({
+                        name: 'form_input_ID',
+                        title: 'ID&nbsp;&nbsp;&nbsp;&nbsp;',
+                        holder: '请输入ID...',
+                        divName: 'input',
+                        type: 'text',
+                        className: 'span4'
+                    });
+    formFirstlist.push({
+                        name: 'form_input_Name',
+                        title: '名称&nbsp;&nbsp;&nbsp;&nbsp;',
+                        holder: '请输入名称...',
+                        divName: 'input',
+                        type: 'text',
+                        className: 'span4'
+                    });
+    formFirstlist.push({
+                        name: 'form_input_Info',
+                        title: '信息&nbsp;&nbsp;&nbsp;&nbsp;',
+                        holder: '请输入信息...',
+                        divName: 'textarea',
+                        type: 'textarea',
+                        className: '"span4 area"'
+                    });
+    var buttonlist=new Array();
+    buttonlist.push({
+                    name: 'form_button_ok',
+                    title: '确定',
+                    divName: 'button',
+                    className: '"btn btn-info"'
+                });
+    buttonlist.push({
+                    name: 'form_button_cancel',
+                    title: '取消',
+                    divName: 'button',
+                    className: '"btn btn-warning"'
+                });
+    formFirstlist.push({
+                    name: 'form_buttons',
+                    divName: 'buttons',
+                    children: buttonlist,
+                });
+   var dataJson= [{
+                    name: 'menu_edit',
+                    title: this.title,
+                    divName: 'div',
+                    children: formFirstlist,
+                    className: '"well span4"'
+                }];
+    return dataJson;
+}
---
+++ js/dynamicForm/dynamicFormProcess.js
@@ -0,0 +1,47 @@
+
+dynamicForm.prototype.fillActioin = function (){
+    var okButton = document.getElementById('form_button_ok');
+    var cancelButton = document.getElementById('form_button_cancel');
+    var titleButton = document.getElementById('form_title_new');
+    okButton.parent = this;
+    cancelButton.parent = this;
+    titleButton.parent = this;
+    
+    okButton.onclick = this.process;
+    cancelButton.onclick = this.cancel;
+    titleButton.onmousedown = this.DragTitle;
+    titleButton.onmouseup = this.unDragTitle;
+    titleButton.onmouseover = this.focusTitle;
+    titleButton.onmouseout = this.unfocusTitle;
+ }
+dynamicForm.prototype.process = function()
+{
+    console.log('处理: ');
+}
+dynamicForm.prototype.cancel = function()
+{
+    this.parent.hide();
+    console.log('取消: ');
+}
+dynamicForm.prototype.DragTitle = function(event)
+{
+    this.style.cursor="move";
+    this.parent.fadeTool.clickOnDiv(this.parent.div,event);
+    console.log('点击: ');
+}
+dynamicForm.prototype.focusTitle = function(event)
+{
+    this.style.cursor="move";
+    console.log('进入: ');
+}
+dynamicForm.prototype.unfocusTitle = function(event)
+{
+    this.style.cursor="default";
+    console.log('进入: ');
+}
+dynamicForm.prototype.unDragTitle = function()
+{
+    this.parent.fadeTool.onUpDiv();
+    this.style.cursor="default";
+    console.log('释放: ');
+}
